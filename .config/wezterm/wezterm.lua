d1aedd15f11abab0dc68aeb23abf85d24f57894b
yuya-toyoizumi <gbr.eng123@gmail.com>
2023-12-16 18:07:19 +0900

    add key to toggle full screen

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,16 @@
+local wezterm = require 'wezterm'
+
 local config = {
   default_prog = { "/opt/homebrew/bin/fish", "-l" },
   window_decorations = 'RESIZE',
   window_background_opacity = 0.94,
+  keys = {
+    {
+      key = 'n',
+      mods = 'SHIFT|CTRL',
+      action = wezterm.action.ToggleFullScreen,
+    },
+  }
 }
 
 return config
