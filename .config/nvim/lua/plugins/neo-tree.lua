718752d65c91417c38278b0c15be0d1377168bc1
yuya-toyoizumi <gbr.eng123@gmail.com>
2023-12-17 14:47:39 +0900

    show dotfiles and gitignored

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,14 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function ()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    }
   end,
 }
